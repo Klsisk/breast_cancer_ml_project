04677e6187ac2f709d89bcf819d7f17722f847f1
Kendall Sisk <69765842+Klsisk@users.noreply.github.com>
2021-03-08 12:15:50 -0500

    updated files

--- Data/breast_cancer.sql
+++ Data/breast_cancer.sql
@@ -1,15 +1,12 @@
-CREATE TABLE cancerData(
-id TEXT,
+CREATE TABLE cancerdata(
 clump_thickness INT,
 size_uniformity INT,
 shape_uniformity INT,
 marginal_adhesion INT,
 epithelial_size INT,
-bare_nucleoli TEXT,
+bare_nucleoli FLOAT,
 bland_chromatin INT,
 normal_nucleoli INT,
 mitoses INT,
 class INT)
 ;
-
-SELECT * FROM cancerData;
