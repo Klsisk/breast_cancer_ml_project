40d21975fbfb6373ffd8e9b818a2b68dfaa6d977
Kendall Sisk <69765842+Klsisk@users.noreply.github.com>
2021-03-14 18:36:45 -0400

    final commit

--- BCData/breast_cancer1.sql
+++ BCData/breast_cancer1.sql
@@ -1,36 +1,6 @@
-CREATE TABLE cancerdata(
-id INT,
-diagnosis TEXT,
-radius_mean FLOAT,
-texture_mean FLOAT,
-perimeter_mean FLOAT,
-area_mean FLOAT,
-smoothness_mean FLOAT,
-compactness_mean FLOAT,
-concavity_mean FLOAT,
-concave_points_mean FLOAT,
-symmetry_mean FLOAT,
-fractal_dimension_mean FLOAT,
-radius_se FLOAT,
-texture_se FLOAT,
-perimeter_se FLOAT,
-area_se FLOAT,
-smoothness_se FLOAT,
-compactness_se FLOAT,
-concavity_se FLOAT,
-concave_points_se FLOAT,
-symmetry_se FLOAT,
-fractal_dimension_se FLOAT,
-radius_worst FLOAT,
-texture_worst FLOAT,
-perimeter_worst FLOAT,
-area_worst FLOAT,
-smoothness_worst FLOAT,
-compactness_worst FLOAT,
-concavity_worst FLOAT,
-concave_points_worst FLOAT,
-symmetry_worst FLOAT,
-fractal_dimension_worst FLOAT)
-;
+select * from cancerdata;
 
-SELECT * FROM cancerdata;
+COPY canderdata(diagnosis, radius_mean, texture_mean, perimeter_mean, area_mean, smoothness_mean, compactness_mean, concavity_mean, concave_points_mean, symmetry_mean, fractal_dimension_mean)
+FROM 'C:\Users\klsis\Google Drive\Homework\breast_cancer_project\BCData\breast_cancer.csv'
+DELIMITER ','
+CSV HEADER;
